10338a125765cbac8973cd8eb0aac9d02bc7c220
Petyo Mitkov <petyo.mitkov@gmail.com>
2017-11-03 15:30:11 +0200

    Finished Step 8-Templating Links & Images / ng-src

--- app/phone-list/phone-list.component.js
+++ app/phone-list/phone-list.component.js
@@ -5,14 +5,12 @@ angular.
   module('phoneList').
   component('phoneList', {
     templateUrl: 'phone-list/phone-list.template.html',
-    controller: ['$http', 
-      function PhoneListController($http) {
-        var self = this;
-        self.orderProp = 'age';
+    controller: ['$http', function PhoneListController($http) {
+      var self = this;
+      self.orderProp = 'age';
 
-        $http.get('phones/phones.json').then(function(response) {
-          self.phones = response.data.slice(0, 5);
-        });
-      }
-    ]
+      $http.get('phones/phones.json').then(function(response) {
+        self.phones = response.data;
+      });
+    }]
   });
